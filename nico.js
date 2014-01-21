973982b690f4b61599e1597bd089cfc56cee6d48
kytiken <kytiken@gmail.com>
2014-01-22 04:01:30 +0900

    動画リストの取得方法を変更

--- nico.js
+++ nico.js
@@ -1,31 +1,35 @@
 "use strict";
-
-var doc = document;
-var isClicked = false;
-
-window.onclick = function(){
-  if(!isClicked){
-    var nicovideo = new NicoVideo(doc, NicoVideo.mylist_page_type);
-    nicovideo.addNicosoundButton();
-  }
-  isClicked = true;
+var NicoVideo = function(video_node_list){
+  this.video_node_list = video_node_list;
 };
 
-var NicoVideo = function(doc, page_type){
-  this.video_node_list = doc.getElementsByClassName("watch");
-  this.page_type = page_type;
-};
-
-NicoVideo.mylist_page_type = "mylist"
+NicoVideo.smile_video_url_pattern = /watch\/sm\d*/
+NicoVideo.nmm_video_url_pattern = /watch\/nm\d*/
+NicoVideo.ch_video_url_pattern = /watch\/\d*/
 
 NicoVideo.prototype.addNicosoundButton = function(){
   //動画リストにnicosoundボタンをつける
   for (var i = 0; i < this.video_node_list.length; i++){
     var origin_video_node = this.video_node_list[i];
-    var nicosound = new NicoSound(origin_video_node.href);
+    var nicosound = new NicoSound(NicoVideo.getVideoUrl(origin_video_node));
     var nicosound_button = nicosound.createNicosoundButton();
-    origin_video_node.parentNode.parentNode.appendChild(nicosound_button);
+    origin_video_node.appendChild(nicosound_button);
   }
+};
+
+NicoVideo.getVideoUrl = function(video_node){
+  var a_tag_nodes = video_node.getElementsByTagName('a');
+  var video_url;
+  for (var i = 0; i < a_tag_nodes.length; i++){
+    if(NicoVideo.smile_video_url_pattern.test(a_tag_nodes[i].href)){
+      video_url = a_tag_nodes[i].href;
+    } else if(NicoVideo.nmm_video_url_pattern.test(a_tag_nodes[i].href)) {
+      video_url = a_tag_nodes[i].href;
+    } else if(NicoVideo.ch_video_url_pattern.test(a_tag_nodes[i].href)) {
+      video_url = a_tag_nodes[i].href;
+    }
+  }
+  return video_url;
 };
 
 var NicoSound = function(video_url){
