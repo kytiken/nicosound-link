3c42b50b053256fcb7b4eb8e49082701678cef3d
kytiken <kytiken@gmail.com>
2014-01-21 21:39:45 +0900

    使っていないメソッドを削除

--- nico.js
+++ nico.js
@@ -15,8 +15,8 @@ function addNicosoundButton(video_node_list){
   for (var i = 0; i < video_node_list.length; i++){
     var origin_video_node = video_node_list[i];
     var button = new Button(origin_video_node);
-    var nsButton = button.createNsButton();
-    origin_video_node.parentNode.parentNode.appendChild(nsButton);
+    var nicosound_button = button.createNsButton();
+    origin_video_node.parentNode.parentNode.appendChild(nicosound_button);
   }
 }
 
@@ -37,19 +37,6 @@ Button.prototype.createNsButton = function(){
     window.open(nicosound);
   };
   return nsButton;
-};
-
-Button.prototype.createPushButton = function() {
-  var pushButton = doc.createElement("button");
-  var text = doc.createTextNode("push");
-  pushButton.appendChild(text);
-  pushButton.onclick = function(){
-    var sm = localStorage.getItem("sm");
-    sm = toArray(sm);
-    sm.push(this.originTagNode);
-    localStorage.setItem("sm",sm);
-  };
-  return pushButton;
 };
 
 function createSoundUrl( sourcePathname ) {
